f8bdf84d115d31a264842247b259b90357d81db2
BadzeGIT <badze@free.fr>
2018-06-26 17:44:54 +0200

    ajout2

--- main.py
+++ main.py
@@ -7,18 +7,12 @@ from Collaborateur import *
 
 
 
-h1 = Collaborateur("Billiard","Fabien","Hotliner","fabien.billiard@itga.fr","0223440735","Super Cool ce mec", "ITGA", "Boite cool")
+h1 = Collaborateur("Zobien","Fabine","Hotliner","Fabine.Zobien@xxxx.fr","0223440832","Super Cool ce mec", "ITGA", "Boite cool")
 
 
 
 h1.voirCollaborateur()
 
-h1.informationEntrep = "ZOB"
+"""h1.informationEntrep = "ZOB"
+h1.voirCollaborateur()"""
 
-h1.voirCollaborateur()
-
-"""
-h2 = Entreprise("ZOBTEAM","balbla")
-print(h2.raisonSociale)
-h2.raisonSociale = "aaa"
-print(h2.raisonSociale)"""

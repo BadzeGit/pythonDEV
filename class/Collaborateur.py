f0a5b5fc5f4bbf5cb66ac9c07282e22b0c29c7b5
BadzeGit <badze@free.fr>
2018-06-27 07:07:47 +0200

    ajout

--- class/Collaborateur.py
+++ class/Collaborateur.py
@@ -10,8 +10,6 @@ class Collaborateur(Entreprise):
 		# Importation de la classe mère Entreprise
 		Entreprise.__init__(self, raisonSociale, informationEntrep)
 
-		print("Création du collaborateur")
-
 		self.__nom = nom
 		self.__prenom = prenom
 		self.__poste = poste
@@ -24,11 +22,9 @@ class Collaborateur(Entreprise):
 	#######################################
 
 	def __getnom(self):
-		print("Récupération de la valeur 'nom'")
 		return self.__nom
 	
 	def __setnom(self, setNom):
-		print("Modification de la valeur 'nom'")
 		self.__nom = setNom
 
 	nom = property(__getnom, __setnom)
@@ -38,11 +34,9 @@ class Collaborateur(Entreprise):
 	#######################################
 
 	def __getprenom(self):
-		print("Récupération de la valeur 'prenom'")
 		return self.__prenom
 
 	def __setprenom(self, setprenom):
-		print("Modification de la valeur 'prénom'")
 		self.__prenom = setprenom
 
 	prenom = property(__getprenom, __setprenom)
@@ -52,11 +46,9 @@ class Collaborateur(Entreprise):
 	#######################################
 
 	def __getposte(self):
-		print("Récupération de la valeur 'poste'")
 		return self.__poste
 
 	def __setposte(self, setposte):
-		print("Modification de la valeur 'poste'")
 		self.__poste = setposte
 
 	poste = property(__getposte, __setposte)
@@ -66,11 +58,9 @@ class Collaborateur(Entreprise):
 	#######################################
 
 	def __getmail(self):
-		print("Récupération de la valeur 'mail'")
 		return self.__mail
 
 	def __setmail(self, setmail):
-		print("Modification de la valeur 'mail'")
 		self.__mail = setmail
 
 	mail = property(__getmail, __setmail)
@@ -80,11 +70,9 @@ class Collaborateur(Entreprise):
 	#######################################
 
 	def __gettelephone(self):
-		print("Récupération de la valeur 'telephone'")
 		return self.__telephone
 
 	def __settelephone(self, settelephone):
-		print("Modification de la valeur 'telephone'")
 		self.__telephone = settelephone
 
 	telephone = property(__gettelephone, __settelephone)
@@ -94,11 +82,9 @@ class Collaborateur(Entreprise):
 	###############################################
 
 	def __getinformationCollab(self):
-		print("Récupération de la valeur 'informationCollab'")
 		return self.__informationCollab
 
 	def __setinformationCollab(self, setinformationCollab):
-		print("Modification de la valeur 'informationCollab'")
 		self.__informationCollab = setinformationCollab
 
 	informationCollab = property(__getinformationCollab, __setinformationCollab)
--- class/Entreprise.py
+++ class/Entreprise.py
@@ -4,7 +4,6 @@
 class Entreprise(object):
 
 	def __init__(self, raisonSociale, informationEntrep):
-		print("Création de l' entreprise")
 		self.__raisonSociale = raisonSociale
 		self.__informationEntrep = informationEntrep
 
@@ -13,12 +12,10 @@ class Entreprise(object):
 	#######################################
 
 	def __getraisonSociale(self):
-		print("Récupération de la valeur 'raisonSociale'") 
 		return self.__raisonSociale
 
 
 	def __setraisonSociale(self, setraisonSociale):
-		print("Modification de la valeur 'raisonSociale'")
 		self.__raisonSociale = setraisonSociale
 
 	raisonSociale = property(__getraisonSociale,__setraisonSociale)
@@ -28,12 +25,10 @@ class Entreprise(object):
 	##########################################
 
 	def __getinformationEntrep(self):
-		print("Récupération de la valeur 'informationEntrep'") 
 		return self.__informationEntrep
 
 
 	def __setinformationEntrep(self, setinformationEntrep):
-		print("Modification de la valeur 'informationEntrep'")
 		self.__informationEntrep = setinformationEntrep
 
 	informationEntrep = property(__getinformationEntrep,__setinformationEntrep)
